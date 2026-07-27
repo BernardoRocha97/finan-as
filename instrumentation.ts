f37f132c9e3e18a103b045fca5581ea4d8365b78
BernardoRocha97 <bernardorocha97@gmail.com>
2026-07-27 14:09:24 +0100

    fix: disable automatic email report on server start

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,1 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { enviarRelatorio } = await import("./lib/email");
-    enviarRelatorio().catch((e) => console.error("[relatório] Erro ao enviar:", e));
-  }
-}
+export async function register() {}
